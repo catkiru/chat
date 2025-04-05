6482b7301304e0d803b0a4fce0dfd272fdec9f2b
katya <cat.kiru@gmail.com>
2025-04-05 14:45:48 +0300

    simple server

--- client.cpp
+++ client.cpp
@@ -1,3 +1,32 @@
+#include <iostream>
+#include <qcoreapplication.h>
+#include <QTcpSocket>
+
 int main(int argc, char *argv[]) {
+    const int port = 1234;
+    QCoreApplication a(argc, argv);
+
+    QTcpSocket socket;
+
+    socket.connectToHost("127.0.0.1", 1234);
+
+    if (!socket.waitForConnected(3000)) {
+        std::cerr << "Could not connect to server" << std::endl;
+        return 1;
+    }
+
+    std::cout << "Connection to server succefully established" << std::endl;
+
+    std::string message = "hello world";
+
+    socket.write(message.c_str());
+    // socket.flush();
+
+    if (!socket.waitForBytesWritten(3000)) {
+        std::cerr << "Fail to send message" << std::endl;
+    } else {
+        std::cout << "Message sended" << std::endl;
+    }
+
     return 0;
 }
--- main.cpp
+++ main.cpp
@@ -2,28 +2,56 @@
 #include <QApplication>
 #include <QPushButton>
 #include <QTcpServer>
+#include <QTcpSocket>
 
 
 class ChatServer : public QTcpServer {
 public:
     using QTcpServer::QTcpServer;
-protected:
 
+protected:
+    void incomingConnection(qintptr handle) override {
+        auto *socket = new QTcpSocket(this);
+        socket->setSocketDescriptor(handle);
+        std::cout << "New connection: " << handle << std::endl;
+
+        connect(socket, &QTcpSocket::readyRead, [socket]() {
+            std::cout << "Incoming message: " << socket->readAll().toStdString() << std::endl;
+        });
+
+        connect(socket, &QTcpSocket::disconnected, [socket]() {
+            std::cout << "Client disconnectd" << socket->socketDescriptor() << std::endl;
+        });
+
+        socket->write("Hello client");
+    }
 };
 
 int main(int argc, char *argv[]) {
-    std::cout << "Starting server";
+    std::cout << "Starting server" << std::endl;
     QApplication a(argc, argv);
 
     int port = 1234;
 
+    if (argc > 1) {
+        try {
+            port = std::stoi(argv[1]);
+        }
+        catch (std::exception &e) {
+            std::cerr << "Exception: " << e.what() << std::endl;
+        }
+        catch (...) {
+            std::cerr << "something wents wrong" << std::endl;
+        }
+    }
+
     ChatServer server;
     if (!server.listen(QHostAddress::Any, port)) {
-        std::cerr << "Cant start server";
+        std::cerr << "Cant start server" << std::endl;
         return 1;
     }
 
-    std::cout << "start litening server on port: " << port << std::endl;
+    std::cout << "start listening server on port: " << port << std::endl;
 
     return QApplication::exec();
 }
